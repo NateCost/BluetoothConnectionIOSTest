1e1cba7225f60e6aaa6626589ae63df83b835398
NateCost <ilya.sakalou@gmail.com>
2020-10-15 09:59:21 +0300

    [add] plain empty project

---
+++ BluetoothConnectTest/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  BluetoothConnectTest
+//
+//  Created by Ilya Sakalou on 10/15/20.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+}
+
---
+++ BluetoothConnectTest/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  BluetoothConnectTest
+//
+//  Created by Ilya Sakalou on 10/15/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+}
+
