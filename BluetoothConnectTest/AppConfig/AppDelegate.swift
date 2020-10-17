948cb1838016c8773063340e8f7489c3c4cae041
NateCost <ilya.sakalou@gmail.com>
2020-10-17 23:37:00 +0300

    [add] programmatic layout splash screen setup

--- BluetoothConnectTest/AppConfig/AppDelegate.swift
+++ BluetoothConnectTest/AppConfig/AppDelegate.swift
@@ -9,8 +9,22 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+  
   func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool { true }
+  ) -> Bool {
+    window = UIWindow(frame: UIScreen.main.bounds)
+    
+    if let window = window {
+      let splash = SplashVC()
+      
+      window.rootViewController = splash
+      window.makeKeyAndVisible()
+      window.backgroundColor = .white
+    }
+    
+    return true
+  }
 }
--- BluetoothConnectTest/Presentation/Splash/SplashVC.swift
+++ BluetoothConnectTest/Presentation/Splash/SplashVC.swift
@@ -6,5 +6,32 @@
 import UIKit
 
 final class SplashVC: UIViewController {
+  lazy var logoImageView: UIImageView = {
+    let logoImageView = UIImageView(frame: .zero)
+    logoImageView.translatesAutoresizingMaskIntoConstraints = false
+    return logoImageView
+  }()
   
+  override func loadView() {
+    super.loadView()
+    view.addSubview(logoImageView)
+    layoutLogo()
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    logoImageView.backgroundColor = .red
+    view.backgroundColor = .white
+  }
+}
+// MARK: - Layout
+private extension SplashVC {
+  func layoutLogo() {
+    NSLayoutConstraint.activate([
+      logoImageView.widthAnchor.constraint(equalToConstant: 100),
+      logoImageView.heightAnchor.constraint(equalTo: logoImageView.widthAnchor),
+      logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      logoImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+    ])
+  }
 }
