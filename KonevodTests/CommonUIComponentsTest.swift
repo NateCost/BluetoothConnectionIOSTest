c379fd3bef84101934bc935a9ca00fe9f08e917e
NateCost <ilya.sakalou@gmail.com>
2020-11-08 12:25:38 +0300

    update test folder

---
+++ KonevodTests/CommonUIComponentsTest.swift
@@ -0,0 +1,31 @@
+//
+//  Created by Ilya Sakalou on 11/3/20.
+//  Copyright © 2020 Nirma Studio. All rights reserved.
+//
+
+import XCTest
+@testable import Konevod
+
+final class CommonUIComponentsTest: XCTestCase {}
+// MARK: - SelectionButtonTest
+extension CommonUIComponentsTest {
+  func testSelectableButtonDisableAutoresizingMask() {
+    let sut = SelectableButton(frame: .zero)
+    XCTAssertFalse(sut.translatesAutoresizingMaskIntoConstraints)
+  }
+  
+  func testSelectableButtonDisableAutoresizingMask2() {
+    let sut = SelectableButton()
+    XCTAssertFalse(sut.translatesAutoresizingMaskIntoConstraints)
+  }
+  
+  func testSelectionButtonActivatesOnTap() {
+    let button = SelectableButton()
+    let sut = ActivatableItemControl(itemStateUpdateHandler: button.setState(_:))
+    sut.action = { _ in }
+   
+    button.sendActions(for: .touchUpInside)
+    
+    XCTAssertEqual(sut.state, .active)
+  }
+}
---
+++ KonevodTests/Splash/SplashPresenterTests.swift
@@ -0,0 +1,34 @@
+//
+//  Created by Ilya Sakalou on 10/31/20.
+//  Copyright © 2020 Nirma Studio. All rights reserved.
+//
+
+import XCTest
+@testable import Konevod
+
+class SplashPresenterTest: XCTestCase {
+  var view: SplashViewInput?
+  
+  func testViewDidLoadActivateLoading() {
+    let sut = makeSUT()
+    sut.viewDidLoad()
+    XCTAssertEqual(sut.view.loadController!.state, .active)
+  }
+  
+  func makeSUT(view: SplashViewInput? = SplashViewSpy()) -> SplashPresenter {
+    self.view = view
+    self.view!.loadController = ActivatableItemControl(itemStateUpdateHandler: ActivatableItemSpy().setState(_:))
+    let sut = SplashPresenter(view: self.view!)
+    
+    return sut
+  }
+  
+  final class SplashViewSpy: SplashViewInput {
+    var loadController: ActivatableItem?
+  }
+  
+  final class ActivatableItemSpy: Selectable {
+    var tapAction: Handler?
+    func setState(_ state: ActivationState) {}
+  }
+}
