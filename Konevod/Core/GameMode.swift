b8c757843c66a0ddf54a66b32277adc9b6310b1f
Ilya Sakalou <ilya.sokolov@orangesoft.by>
2021-01-25 22:44:08 +0300

    add GameMode

---
+++ Konevod/Core/GameMode.swift
@@ -0,0 +1,13 @@
+//
+//  Created by Ilya Sakalou on 25.01.21.
+//  Copyright © 2021 Nirma Studio. All rights reserved.
+//
+
+import Foundation
+
+public enum GameMode {
+  case casual
+  case seasonalCasual
+  case hardcore
+  case seasonalHardcore
+}
